d85593c235cdc57cf0d2e3bea138fba27e8b1429
lonelylonelylonely <mirmir730@naver.com>
2024-09-10 12:29:40 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,12 @@ def print_hello():
 if __name__ == '__main__asdasd':
     print_hello()
     print("aaaaa")
+
+def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print(“KIT")
+if __name__ == '__main__’:
+print_KIT()
+print( print_hello(1,2) )
